cbe7facf539d7545004996403a62971dccf474fa
Shane <shanemcniff@gmail.com>
2017-02-08 11:40:52 +0000

    Trying to set everything back to working order

--- app/src/main/java/com/example/shane/MAV/StartActivity.java
+++ app/src/main/java/com/example/shane/MAV/StartActivity.java
@@ -7,19 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    EditText Email, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-
-
-
-        ((TextView)findViewById(R.id.LoginConfirm)).setText("");
+        Email = (EditText)findViewById(R.id.input_email);
+        Password = (EditText)findViewById(R.id.input_password);
     }
 
     public void onLogin(View view){
