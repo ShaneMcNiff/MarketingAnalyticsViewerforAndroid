84fc012b6e5ed760e267b452d315e6791eae10e1
Shane <shanemcniff@gmail.com>
2017-04-03 12:51:31 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/shane/MAV/BackgroundWorker.java
    #	app/src/main/java/com/example/shane/MAV/CampaignScreen.java
    #	app/src/main/java/com/example/shane/MAV/StartActivity.java
    #	app/src/main/res/layout/activity_campaign_screen.xml
    #	app/src/main/res/layout/activity_campaign_summary.xml
    #	app/src/main/res/layout/content_campaign_selection.xml

--- app/src/main/java/com/example/shane/MAV/Objects/User.java
+++ app/src/main/java/com/example/shane/MAV/Objects/User.java
@@ -9,7 +9,7 @@ import com.example.shane.MAV.Interfaces.usersInterfce;
  * Created by Shane on 22/12/2016.
  */
 
-public class User implements usersInterfce, Parcelable{
+public class User implements usersInterfce{
 
     private String email;
     private String name;
@@ -26,25 +26,6 @@ public class User implements usersInterfce, Parcelable{
         this.password = password;
         this.is_Admin = is_Admin;
     }
-
-    protected User(Parcel in) {
-        name = in.readString();
-        email = in.readString();
-        password = in.readString();
-        is_Admin = in.readInt();
-    }
-
-    public static final Creator<User> CREATOR = new Creator<User>() {
-        @Override
-        public User createFromParcel(Parcel in) {
-            return new User(in);
-        }
-
-        @Override
-        public User[] newArray(int size) {
-            return new User[size];
-        }
-    };
 
     public String getPassword(){
         return password;
@@ -66,17 +47,4 @@ public class User implements usersInterfce, Parcelable{
             is_Admin = 0;
     }
 
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeString(email);
-        dest.writeString(password);
-        dest.writeInt(is_Admin);
-    }
 }
